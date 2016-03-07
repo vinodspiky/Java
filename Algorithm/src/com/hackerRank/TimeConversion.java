1545bf10c9af7fdda935f2b4f19f802957acb828
VINOD CHOUDHARY <vinodspiky@gmail.com>
2016-03-08 02:57:19 +0530

    Time Conversion

---
+++ Algorithm/src/com/hackerRank/TimeConversion.java
@@ -0,0 +1,24 @@
+package com.hackerRank;
+
+import java.util.Scanner;
+
+public class TimeConversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	        Scanner in = new Scanner(System.in);
+	        String time = in.next();
+	        String[] component=time.split(":");
+	        int hrs=Integer.parseInt(component[0])+12;
+	        component[0]=""+ hrs;
+	        component[2]=component[2].replace("P", "");
+	        //component[2].replace("A", "");
+	       // component[2].replace("M", "");
+	        
+	        System.out.println(component[2]);
+	  
+
+	}
+
+}
